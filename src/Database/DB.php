ffff7023f867fb20e1ac36f5a99bd9b3a96c4a8b
Yash Patel <yash.p@zivame.com>
2020-05-10 12:08:51 +0530

    commented proper methods

--- src/Database/DB.php
+++ src/Database/DB.php
@@ -2,15 +2,33 @@
 
 namespace Database;
 
+use BadMethodCallException;
+
 class DB
 {
 	protected static $allowedStaticMethods = ['table'];
 
-	public static function getQueryBuilder() {
+	/**
+	 * Gives QueryBuilder's instance
+	 * @return \Database\QueryBuilder
+	 */
+	protected static function getQueryBuilder() {
 		return new QueryBuilder;
 	}
 
+	/**
+	 * It'll be responsilbe to handle undefined
+	 * static methods
+	 * @param  string $method
+	 * @param  array $params
+	 * @return mixed
+	 */
 	public static function __callStatic($method, $params) {
+		if (! in_array(self::allowedStaticMethods, $method)) {
+			throw new BadMethodCallException("Static method {$method} not found.");
+
+		}
+
 		return self::getQueryBuilder()->$method(...$params);
 	}
 }
