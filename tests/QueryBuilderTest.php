b9b5323dd2324a15ad769e4c2efb48ce2fd2b000
Yash Patel <yash.p@zivame.com>
2020-05-10 08:46:12 +0530

    added select query builder

--- tests/QueryBuilderTest.php
+++ tests/QueryBuilderTest.php
@@ -10,24 +10,32 @@ class DatabaseManageTests extends TestCase
 	/** @test  */
 	public function it_can_build_sql_statement()
 	{
-		$selectQuery = (new QueryBuilder)->table('users')->get();
+		$sql = (new QueryBuilder)->table('users')->get();
 
-		$this->assertEquals($selectQuery, 'SELECT `*` FROM `users`');
+		$this->assertEquals($sql, 'SELECT `*` FROM `users`');
 	}
 
 	/** @test  */
 	public function it_can_select_on_specific_columns()
 	{
-		$selectSpecificColumns = (new QueryBuilder)->table('users')->select('name', 'email')->get();
+		$sql = (new QueryBuilder)->table('users')->select('name', 'email')->get();
 
-		$this->assertEquals($selectSpecificColumns, 'SELECT `name`, `email` FROM `users`');
+		$this->assertEquals($sql, 'SELECT `name`, `email` FROM `users`');
 	}
 
 	/** @test  */
 	public function it_can_limit_on_the_results()
 	{
-		$limitQuery = (new QueryBuilder)->table('users')->limit(10)->get();
+		$sql = (new QueryBuilder)->table('users')->limit(10)->get();
 
-		$this->assertEquals($limitQuery, 'SELECT `*` FROM `users` LIMIT 10');
+		$this->assertEquals($sql, 'SELECT `*` FROM `users` LIMIT 10');
+	}
+
+	/** @test  */
+	public function it_can_select_specific_columns_as_well_as_limit_on_it()
+	{
+		$sql = (new QueryBuilder)->table('users')->select('name', 'email')->limit(10)->get();
+
+		$this->assertEquals($sql, 'SELECT `name`, `email` FROM `users` LIMIT 10');
 	}
 }
