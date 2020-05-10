110aa4be1af2bcdb83ed015584a226ce06f33a37
Yash Patel <yash.p@zivame.com>
2020-05-10 08:11:53 +0530

    created project from scratch

--- tests/DatabaseManageTests.php
+++ tests/DatabaseManageTests.php
@@ -1,5 +1,6 @@
 <?php
 
+use Database\QueryBuilder;
 use PHPUnit\Framework\TestCase;
 
 declare(strict_types=1);
@@ -7,8 +8,10 @@ declare(strict_types=1);
 class DatabaseManageTests extends TestCase
 {
 	/** @test  */
-	public function it_has_ability_do_select_statement()
+	public function can_build_sql_statement()
 	{
+		$selectQuery = QueryBuilder::table('users')->get();
 
+		$this->assertEquals($selectQuery, 'SELECT * FROM `users`');
 	}
 }
