66dbb855cc4428b4948cd67dbdd2c55ebe4fd60f
Yash Patel <yash.p@zivame.com>
2020-05-10 08:01:39 +0530

    created project from scratch

---
+++ tests/DatabaseManageTests.php
@@ -0,0 +1,14 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+declare(strict_types=1);
+
+class DatabaseManageTests extends TestCase
+{
+	/** @test  */
+	public function it_has_ability_do_select_statement()
+	{
+
+	}
+}
